934aa0a1f9c06a47eba49c7ccfb8fec0fa931729
Applikationsprogramvara <applikationsprogramvara@gmail.com>
2021-08-19 22:32:00 +0200

    adding requested super method

--- app/src/main/java/com/applikationsprogramvara/osmviewer/MainActivity.java
+++ app/src/main/java/com/applikationsprogramvara/osmviewer/MainActivity.java
@@ -448,6 +448,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
             switch (requestCode) {
                 case REQUEST_LOCATION_JUMP:
